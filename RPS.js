f1357299b99b02149f1ac8ead7db4b6b0a4a6b43
Robert Erickson <bobericksonjr@gmail.com>
2021-04-07 10:53:38 -0400

    adds working javascript and basic interface

--- RPS.js
+++ RPS.js
@@ -6,7 +6,7 @@ function computerPlay() {
         let min = Math.ceil(0);
         let max = Math.floor(2);
         return Math.floor(Math.random() * (max - min + 1));
-    } 
+    }
 
     // stores result of random number generator into variable
     let getRandom = randomGenerator();
@@ -21,79 +21,147 @@ function computerPlay() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection.toLowerCase(); // lowercase player's selection
+function playRound(playerSelection) {
+    console.log(playerSelection)
+    const computerSelection = computerPlay();
+    console.log(computerSelection)
+
+    playerSelection = playerSelection.toLowerCase(); // lowercase player's selection for str compare
+
+    let roundResult = "";
+
     if (playerSelection === computerSelection) {
-        return "tie";
+        roundResult = "tie";
     } else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
-            return "lose";
+            roundResult = "lose";
         } else { //computer must have chose scissors
-            return "win";
+            roundResult = "win";
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'scissors') {
-            return "lose";
+            roundResult = "lose";
         } else { // computer must have chose rock
-            return "win";
+            roundResult = "win";
         }
     } else { // player must have chose scissors
         if (computerSelection === 'rock') {
-            return "lose";
+            roundResult = "lose";
         } else { // computer must have chose paper
-            return "win";
+            roundResult = "win";
+        }
+    }
+
+    document.querySelector('#results').innerText = `Result: You chose ${playerSelection}, `
+        + `your opponent chose ${computerSelection} \u2014 you ${roundResult} the round.`;
+
+    return roundResult;
+}
+
+function endGame(winner) {
+
+    // grab all the game buttons and remove event listener
+    const buttonDiv = document.querySelector('#selection-buttons');
+    const buttons = buttonDiv.querySelectorAll('button');
+    buttons.forEach(button => button.removeEventListener('click', roundClick));
+
+    const endGamePara = document.querySelector('#final-results');
+
+    if (winner === 'player') {
+        
+        endGamePara.innerText = 'Congrats! You won the game!'
+        return;
+    } else {
+        endGamePara.innerText = 'Computer wins! Try again!'
+        return;
+    }
+}
+
+function roundClick(e) {
+    console.log(e.target.innerText)
+    let playerSelection = e.target.innerText;
+    let roundResult = playRound(playerSelection);
+    console.log(roundResult);
+
+    if (roundResult === 'win') {
+        playerScore++;
+        document.querySelector('#player-score').innerText = `Player Score: ${playerScore}`;
+        if (playerScore === 5) {
+            let winner = 'player';
+            endGame(winner);
+        }
+    } else if (roundResult === 'lose') {
+        computerScore++;
+        document.querySelector('#computer-score').innerText = `Computer Score: ${computerScore}`;
+        if (computerScore === 5) {
+            let winner = 'computer';
+            endGame(winner);
         }
     }
 }
+
 
 function game() {
 
+    const playAgain = document.querySelector('#play-again');
+    playAgain.addEventListener('click', game);
+
     // initiate score variables
-    let playerScore = 0;
-    let computerScore = 0;
+    playerScore = 0;
+    computerScore = 0;
+    document.querySelector('#player-score').innerText = `Player Score: ${playerScore}`;
+    document.querySelector('#computer-score').innerText = `Computer Score: ${computerScore}`;
+    document.querySelector('#results').innerText = 'Result:'
 
-    // loop 5 times
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = "";
-        // prompt player for correct input
-        while (playerSelection === "") {
-            playerSelection = prompt("Choose rock, paper, or scissors: ");
-            // lowercase player input
-            playerSelection = playerSelection.toLowerCase();
-            
-            // if player input isn't valid, restart while loop
-            if (playerSelection !== 'rock' && playerSelection !== 'scissors' && playerSelection !== 'paper') {
-                alert("Please choose rock, paper, or scissors!");
-                playerSelection = "";
-            }
-        }
-        // now the computer generates a play
-        let computerSelection = computerPlay() ;
+    const buttonDiv = document.querySelector('#selection-buttons');
+    const buttons = buttonDiv.querySelectorAll('button');
+    console.log(buttons);
 
-        // function to compare plays
-        let turnResult = playRound(playerSelection, computerSelection);
-        
-        if (turnResult === "tie") {
-            console.log(`It's a tie! Both selected ${playerSelection}!`);
-        } else if (turnResult === "win") {
-            playerScore++; //update score
-            console.log(`You win- ${playerSelection} beats ${computerSelection}!`);
-        } else {
-            computerScore++; //update score
-            console.log(`You lose- ${playerSelection} loses to ${computerSelection}!`);
-        } 
-    }
-
-    // compare scores after 5 rounds, output outcome of game
-    if (playerScore > computerScore) {
-        console.log(`You win the game! ${playerScore} to ${computerScore}.`);
-    } else if (playerScore < computerScore) {
-        console.log(`You lose the game! ${playerScore} to ${computerScore}`);
-    } else {
-        console.log(`Tie game! ${playerScore} to ${computerScore}.`);
-    }
-    
-
+    buttons.forEach(button => button.addEventListener('click', roundClick));
 }
 
+// let playerSelection = "";
+// // prompt player for correct input
+// while (playerSelection === "") {
+//     playerSelection = prompt("Choose rock, paper, or scissors: ");
+//     // lowercase player input
+//     playerSelection = playerSelection.toLowerCase();
+
+//     // if player input isn't valid, restart while loop
+//     if (playerSelection !== 'rock' && playerSelection !== 'scissors' && playerSelection !== 'paper') {
+//         alert("Please choose rock, paper, or scissors!");
+//         playerSelection = "";
+//     }
+
+//     // now the computer generates a play
+//     let computerSelection = computerPlay();
+
+//     // function to compare plays
+//     let turnResult = playRound(playerSelection, computerSelection);
+
+//     if (turnResult === "tie") {
+//         console.log(`It's a tie! Both selected ${playerSelection}!`);
+//     } else if (turnResult === "win") {
+//         playerScore++; //update score
+//         console.log(`You win- ${playerSelection} beats ${computerSelection}!`);
+//     } else {
+//         computerScore++; //update score
+//         console.log(`You lose- ${playerSelection} loses to ${computerSelection}!`);
+//     }
+// }
+
+// // compare scores after 5 rounds, output outcome of game
+// if (playerScore > computerScore) {
+//     console.log(`You win the game! ${playerScore} to ${computerScore}.`);
+// } else if (playerScore < computerScore) {
+//     console.log(`You lose the game! ${playerScore} to ${computerScore}`);
+// } else {
+//     console.log(`Tie game! ${playerScore} to ${computerScore}.`);
+// }
+
+
+//}
+
+let playerScore;
+let computerScore ;
 game();
